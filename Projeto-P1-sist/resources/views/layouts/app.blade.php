d19f94519ed9aed973c02aae8d58f1ffb690aaad
AnaCarolinaFerreira-gif <122412749+AnaCarolinaFerreira-gif@users.noreply.github.com>
2024-10-05 08:43:14 -0300

    Adicionando bootstrap ao arquivo app

--- Projeto-P1-sist/resources/views/layouts/app.blade.php
+++ Projeto-P1-sist/resources/views/layouts/app.blade.php
@@ -4,27 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Entrada de clientes</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
 </head>
 <body>
-    <header>
+    <header class="container">
         <h1>Entarada de clientes</h1>
-        <nav>
-            <ul>
-                <li><a href="{{ route( 'clientes.index')}}">Clientes</a></li>
-                <li><a href="{{ route( 'clientes.create')}}">Novo Cliente</a></li>
-            </ul>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">  
+                <ul class="navbar-nav">
+                    <li class="nav-item"> <a href="{{ route( 'clientes.index')}}">Clientes</a></li>
+                    <li class="nav-item"><a href="{{ route( 'clientes.create')}}">Novo Cliente</a></li>
+                </ul>
+            </div>
         </nav>
     </header>
 
-
-    <main>
+    <main class="container">
         @yield('content')
     </main>
 
-    <footer>
-        <p>&copy; {{ date('Y') }} Entrada de clientes.</p>
+    <footer class="continer-md bt-4 bd-black">
+        <p style="color: fff">&copy; {{ date('Y') }} Entrada de clientes.</p>
     </footer>
 </body>
 </html>
