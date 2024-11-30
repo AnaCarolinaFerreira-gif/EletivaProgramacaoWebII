7a0676e32e2f83bbb5a6aec5adb9550506604b6b
AnaCarolinaFerreira-gif <122412749+AnaCarolinaFerreira-gif@users.noreply.github.com>
2024-11-30 07:40:41 -0300

    Update Vagas.php

--- Projeto-P1-sist/app/Models/Vagas.php
+++ Projeto-P1-sist/app/Models/Vagas.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use App\Models\Veiculos;
+use app\Models\Clientes;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Vagas extends Model
 {
-    protected $fillable = ['ocupado','hora_entrada','hora_saida'];
+    protected $fillable = ['ocupado','hora_entrada','hora_saida', 'veiculo_id' ,'cliente_id'];
+
+
+    public function veiculo() {
+        return $this->belongsTo(Veiculos::class);
+
+    }
+
+    public function cliente() {
+        return $this->belongsTo(Clientes::class);
+    }
 }
